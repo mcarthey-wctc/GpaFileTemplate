98a6edf2cb85cbd861b3eb3b28212fee8de458b3
Mark McArthey <mcarthey@gmail.com>
2024-02-03 13:46:24 -0600

    Modified to .Net 8.0 and updated workflow to match; Made test data properties nullable to prevent warnings

--- CourseGPAFile.Tests/Models/TestData.cs
+++ CourseGPAFile.Tests/Models/TestData.cs
@@ -2,6 +2,6 @@ namespace CourseGPAFile.Tests.Models;
 
 public class TestData
 {
-    public string[] Grades { get; set; }
-    public string ExpectedGPA { get; set; }
+    public string[]? Grades { get; set; }
+    public string? ExpectedGPA { get; set; }
 }
